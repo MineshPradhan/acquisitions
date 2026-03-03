03d1779d48c618598f272e0261df57a036c7d492
MineshPradhan <mineshpradhan2002@gmail.com>
2026-03-03 16:29:28 +0530

    fixed

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import logger from '#config/logger.js'  
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from Acquisitions!');
+    logger.info('Hello from Acquisitions!');
+    res.status(200).send('Hello from Acquisitions!');
 });
 
 export default app;
